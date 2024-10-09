25287afe7c7ec186558d4478fb0d884ca130b390
rhit-brookse1 <95264948+rhit-brookse1@users.noreply.github.com>
2024-10-09 05:58:38 -0400

    added a fake database

--- frontend/src/routes/+reportpage.svelte
+++ frontend/src/routes/+reportpage.svelte
@@ -1,6 +1,15 @@
 <script>
+	import { fly, slide } from 'svelte/transition';
+	import { enhance } from '$app/forms';
+
 	export let data;
 	export let form;
+
+	let creating = false;
+	/**
+	 * @type {any[]}
+	 */
+	let deleting = [];
 </script>
 
 <div class="centered">
@@ -10,10 +19,22 @@
 		<p class="error">{form.error}</p>
 	{/if}
 
-	<form method="POST" action="?/create">
+	<form
+		method="POST"
+		action="?/create"
+		use:enhance={() => {
+			creating = true;
+
+			return async ({ update }) => {
+				await update();
+				creating = false;
+			};
+		}}
+	>
 		<label>
 			add a todo:
 			<input
+				disabled={creating}
 				name="description"
 				value={form?.description ?? ''}
 				autocomplete="off"
@@ -23,9 +44,19 @@
 	</form>
 
 	<ul class="todos">
-		{#each data.todos as todo (todo.id)}
-			<li>
-				<form method="POST" action="?/delete">
+		{#each data.todos.filter((/** @type {{ id: any; }} */ todo) => !deleting.includes(todo.id)) as todo (todo.id)}
+			<li in:fly={{ y: 20 }} out:slide>
+				<form
+					method="POST"
+					action="?/delete"
+					use:enhance={() => {
+						deleting = [...deleting, todo.id];
+						return async ({ update }) => {
+							await update();
+							deleting = deleting.filter((id) => id !== todo.id);
+						};
+					}}
+				>
 					<input type="hidden" name="id" value={todo.id} />
 					<span>{todo.description}</span>
 					<button aria-label="Mark as complete"></button>
@@ -33,6 +64,10 @@
 			</li>
 		{/each}
 	</ul>
+
+	{#if creating}
+		<span class="saving">saving...</span>
+	{/if}
 </div>
 
 <style>
