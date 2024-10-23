1a6a270075b50060e4589535df62ef55fbd19b12
rhit-brookse1 <95264948+rhit-brookse1@users.noreply.github.com>
2024-10-23 10:25:59 -0400

    fixed errors in reportpage.server.js

--- frontend/src/routes/+reportpage.server.js
+++ frontend/src/routes/+reportpage.server.js
@@ -1,7 +1,10 @@
 import { fail } from '@sveltejs/kit';
-import * as db from '$lib/database.js';
-console.log(db);
-export function load({ cookies }) {
+import * as db from '$lib/server/database.js';
+
+/**
+ * @param {{ cookies: import('@sveltejs/kit').Cookies }} context
+ */
+export async function load({ cookies }) {
 	let id = cookies.get('userid');
 
 	if (!id) {
@@ -9,27 +12,34 @@ export function load({ cookies }) {
 		cookies.set('userid', id, { path: '/' });
 	}
 
+	const todos = await db.getTodos(id);
 	return {
-		todos: db.getTodos(id)
+		todos
 	};
 }
 
+/**
+ * @type {import('@sveltejs/kit').Actions}
+ */
 export const actions = {
+	
 	create: async ({ cookies, request }) => {
 		const data = await request.formData();
 
 		try {
-			db.createTodo(cookies.get('userid'), data.get('description'));
+			await db.createTodo(cookies.get('userid'), data.get('description'));
 		} catch (error) {
+			// Safe type checking
+            const errorMessage = (error instanceof Error) ? error.message : 'An unknown error occurred';
 			return fail(422, {
 				description: data.get('description'),
-				error: error.message
+				error: errorMessage
 			});
 		}
 	},
 
 	delete: async ({ cookies, request }) => {
 		const data = await request.formData();
-		db.deleteTodo(cookies.get('userid'), data.get('id'));
+		await db.deleteTodo(cookies.get('userid'), data.get('id'));
 	}
 };
