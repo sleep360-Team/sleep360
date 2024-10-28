1cb7e7e24482b8249778a0023871257b7ae40710
rhit-brookse1 <95264948+rhit-brookse1@users.noreply.github.com>
2024-10-28 12:30:58 -0400

    better styling global conventions

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -7,19 +7,3 @@
 <h1>Welcome</h1>
 <p>this is the Main page</p>
 
-<style>
-    :global(body)  {
-        font-family: 'DM Serif Display', serif;
-        background: #DDDDDD;
-    }
-
-    .navbar {
-        background-color: #800000;
-        color: white;
-    }
-    .navbar a {
-            color: white; /* Change link color to white */
-            text-decoration: none; /* Remove underline */
-        }
-
-</style>
--- frontend/src/routes/report/+page.svelte
+++ frontend/src/routes/report/+page.svelte
@@ -1,8 +1,3 @@
-<nav class="navbar navbar-dark fixed-top">
-	<a href="/">home</a>
-	<a href="/report">Report Sleep</a>
-</nav>
-
 <h1>Welcome</h1>
 <p>this is the Report page</p>
 
@@ -83,18 +78,7 @@
 </div>
 
 <style>
-	body {
-    background: #DDDDDD;
-}
-
-.navbar {
-    background-color: #800000;
-    color: white;
-}
-.navbar a {
-        color: white; /* Change link color to white */
-        text-decoration: none; /* Remove underline */
-    }
+	
 
 	.centered {
 		max-width: 20em;
