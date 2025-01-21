f69dcb7c51cb75037611e63c245843072f4e9296
rhit-boykinjt <boykinjt@rose-hulman.edu>
2025-01-21 16:12:21 -0500

    Cookies working again

--- src/lib/server/database.d.ts
+++ src/lib/server/database.d.ts
@@ -27,8 +27,7 @@ declare module '$lib/server/database.js' {
 
 	export function createAccount(
 		username: string,
-		hash: string,
-		id: int
+		hash: string
 	): Promise<ResultSetHeader>;
 
 	export function updateAccount(
