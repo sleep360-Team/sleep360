d2de8d6523017a1d45f3bb9e0cef2deb360254a5
William <greenlee04@gmail.com>
2025-01-28 22:55:21 -0500

    add

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Kill any existing instances of this script
+sudo pkill -f node
+
+# Switch to the main branch
+#git checkout main
+
+# Pull the latest changes
+git pull
+
+# Install dependencies
+npm install
+
+# Build the project
+npm run build
+
+# Run the build
+sudo forever node server.js
