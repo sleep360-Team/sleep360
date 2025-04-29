02f17eaafe3caf44f6020b2c9df9d02ea16e6dab
rhit-hallamcs <95266155+rhit-hallamcs@users.noreply.github.com>
2025-04-28 21:42:49 -0400

    prepped delete requests for merge request

--- src/SQLScripts/storedprocs.sql
+++ src/SQLScripts/storedprocs.sql
@@ -92,3 +92,19 @@ BEGIN
         THROW @ErrorSeverity, @ErrorMessage, @ErrorState;
     END CATCH
 END;
+
+CREATE PROCEDURE [dbo].[DeleteReport]
+(@ReportID int)
+AS
+BEGIN
+
+	IF(NOT EXISTS (SELECT * FROM Reports WHERE @ReportID = ReportID))
+	BEGIN
+		RAISERROR('Report does not exist', 14, 1)
+		RETURN 1
+	END
+
+	DELETE FROM dbo.Reports WHERE ReportID = @ReportID;
+
+	RETURN 0
+END
